6f1df8cd62b432a5c5ef4ce3dbe6bdb6759c0fd2
Arunkumar TS <tsarunkumar2005pc@gmail.com>
2025-04-18 21:12:42 +0530

    update to download resume

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -16,111 +16,131 @@ const Home = () => {
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  
+  const handleDownload = () => {
+    let date = new Date();
+    let fullDate = String(date).slice(0,16)
+    const link = document.createElement('a');
+    link.href = 'Arun ts - Resume.pdf'; 
+    link.download = `Arun ts - Resume ${fullDate}.pdf`;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
+
   return (
     <div style={{ backgroundColor: "rgba(255, 165, 0,0.1)" }}>
       <br />
+      {/* Navbar */}
+      <Container className=" border rounded-5 shadow navbarContainer " style={{ backgroundColor: "white" }}>
+        <Navbar expand={"md"} >
+          <Container >
+            <Navbar.Brand className="brand">
+              <h2 style={{ display: "inline" }}>Arun Kumar ts</h2>
+              <span style={{ color: "blue" }}>.</span>
+            </Navbar.Brand>
+            <Navbar.Toggle
+              aria-controls={`offcanvasNavbar-expand-md`}
+              className="rounded-5 offCanvaButton"
+              onClick={handleShow}
+            />
+            <Navbar.Offcanvas
+              show={show}
+              onHide={handleClose}
+              style={{ width: "70%", backgroundColor: "#FFE6C9" }}
+              aria-labelledby={`offcanvasNavbarLabel-expand-md`}
+              placement="start"
+            >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title className="brand">
+                  Arun Kumar ts<span style={{ color: "blue" }}>.</span>
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Nav className="justify-content-end flex-grow-1 pe-3">
+                  <Nav.Link
+                    href="#homePage"
+                    className="link text-black"
+                    onClick={handleClose}
+                  >
+                    Home
+                  </Nav.Link>
+                  <Nav.Link
+                    href="#aboutMePage"
+                    className="link text-black"
+                    onClick={handleClose}
+                  >
+                    About me
+                  </Nav.Link>
+                  <Nav.Link className="link text-black" onClick={handleClose}>
+                    Projects
+                  </Nav.Link>
+                  <Nav.Link className="link text-black" onClick={handleClose}>
+                    Contact me
+                  </Nav.Link>
+                </Nav>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+            <Button variant="light" className="my-resume d-none d-lg-block" onClick={handleDownload}>
+              My resume <MdDownloading />
+            </Button>
+          </Container>
+        </Navbar>
+      </Container>
 
-      <Navbar expand={"md"} className=" border rounded-5 shadow ">
-        <Container>
-          <Navbar.Brand className="brand">
-            <h3 style={{ display: "inline" }}>Arun Kumar</h3>
-            <span style={{ color: "blue" }}>.</span>
-          </Navbar.Brand>
-          <Navbar.Toggle
-            aria-controls={`offcanvasNavbar-expand-md`}
-            className="rounded-5 offCanvaButton"
-            onClick={handleShow}
-          />
-          <Navbar.Offcanvas
-            show={show}
-            onHide={handleClose}
-            style={{ width: "70%", backgroundColor: "#FFE6C9" }}
-            aria-labelledby={`offcanvasNavbarLabel-expand-md`}
-            placement="start"
-          >
-            <Offcanvas.Header closeButton>
-              <Offcanvas.Title className="brand">
-                Arun Kumar<span style={{ color: "blue" }}>.</span>
-              </Offcanvas.Title>
-            </Offcanvas.Header>
-            <Offcanvas.Body>
-              <Nav className="justify-content-end flex-grow-1 pe-3">
-                <Nav.Link
-                  href="#homePage"
-                  className="link"
-                  onClick={handleClose}
-                >
-                  Home
-                </Nav.Link>
-                <Nav.Link
-                  href="#aboutMePage"
-                  className="link"
-                  onClick={handleClose}
-                >
-                  About me
-                </Nav.Link>
-                <Nav.Link className="link" onClick={handleClose}>
-                  Projects
-                </Nav.Link>
-                <Nav.Link className="link" onClick={handleClose}>
-                  Contact me
-                </Nav.Link>
-              </Nav>
-            </Offcanvas.Body>
-          </Navbar.Offcanvas>
-          <Button variant="light" className="my-resume d-none d-lg-block">
-            My resume <MdDownloading />
-          </Button>
-        </Container>
-      </Navbar>
-
-      <Container id="homePage">
+      {/* MainDetail */}
+      <Container id="homePage" >
         <div className="my-detail">
           <center>
             <Image
               src="portfolio_img.png"
-              width={250}
-              className=" d-block d-md-none mt-5"
+              width={300}
+              className=" d-block d-md-none "
             />
           </center>
-          <h5 id="h5-top">👋 Hi, It's Me</h5>
-          <h2>Arun Kumar ts</h2>
-          <h5>
+          <h3 id="h5-top">👋 Hi, It's Me</h3>
+          <h1>Arun Kumar ts</h1>
+          <h3>
             And I'm a <span>Frontend Developer</span>
-          </h5>
+          </h3>
           <p>
             Lorem ipsum dolor, sit amet consectetur adipisicing elit.
             Perspiciatis debitis voluptates natus provident quidem esse vitae
             modi at animi dignissimos!
           </p>
-          <div>
-            <a className="social-link" href="https://github.com/arunkumar-ts">
+          <div className="mt-4">
+            <a className="social-link shadow" href="https://github.com/arunkumar-ts">
               <FaGithub />
             </a>
             <a
-              className="social-link "
+              className="social-link shadow "
               href="https://www.linkedin.com/in/arunkumar-ts2005"
             >
               <FaLinkedin />
             </a>
-            <a className="social-link " href="https://wa.me/9342772857">
+            <a className="social-link shadow" href="https://wa.me/9342772857">
               <FaWhatsapp />
             </a>
           </div>
-          <Button variant="light" className="my-resume mt-3 me-2 shadow">
+
+          <Button variant="light" className="my-resume mt-4 me-2 shadow">
             contact me <FaArrowRight />
           </Button>
-          <Button variant="light" className="my-resume1 mt-3 shadow">
-            my resume <MdDownloading style={{ color: "orange" }} />
+
+          <Button variant="light" className="my-resume mt-4 shadow" onClick={handleDownload}>
+            my resume <MdDownloading />
           </Button>
+
           <Image
             src="portfolio_img.png"
-            width={250}
+            width={390}
             className="portfolio_img d-none d-md-block "
           />
         </div>
       </Container>
 
+      {/* AboutMe */}
       <Container id="aboutMePage">
         <div className=".about-me-detail">
           <h2>
